aab0f3bc4224689c0038e9d0b15e2e927f3e6479
Wei Han Yau <weihanyau@gmail.com>
2023-05-20 15:12:08 +0800

    Done submit video

--- index.js
+++ index.js
@@ -4,17 +4,26 @@ import {
   createInterview,
   createJob,
   startInterview,
+  submitVideo,
 } from "./service/InterviewService.js";
 import bodyParser from "body-parser";
+import multer from "multer";
+import uuid4 from "uuid4";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 const port = 3000;
+
+const upload = multer({ dest: "/upload" });
 
 app.use(bodyParser.json());
 
 main().catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
+  console.log(uuid4());
   res.send("Hello World!");
 });
 
@@ -39,6 +48,21 @@ app.post("/interview/:id/start", async (req, res) => {
 
   const startedInterview = await startInterview(interviewId);
   return res.send(startedInterview);
+});
+
+app.post("/upload", upload.single("file"), async (req, res) => {
+  const videoFile = req.file;
+  try {
+    const result = await submitVideo(videoFile);
+    if (result === "success") {
+      res.status(200).contentType("text/plain").end("File uploaded!");
+    }
+  } catch (err) {
+    res
+      .status(500)
+      .contentType("text/plain")
+      .end("Oops! Something went wrong!");
+  }
 });
 
 app.listen(port, () => {
--- service/InterviewService.js
+++ service/InterviewService.js
@@ -1,6 +1,27 @@
 import { Job } from "../model/job.js";
 import { Interview } from "../model/interview.js";
 import { InterviewDetail } from "../model/interviewDetail.js";
+import path from "path";
+import fs from "fs";
+import uuid4 from "uuid4";
+import ffmpeg from "fluent-ffmpeg";
+import { SpeechClient } from "@google-cloud/speech";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const client = new SpeechClient();
+const model = "video";
+const encoding = "MP3";
+const sampleRateHertz = 16000;
+const languageCode = "en-US";
+
+const config = {
+  encoding: encoding,
+  sampleRateHertz: sampleRateHertz,
+  languageCode: languageCode,
+  model: model,
+};
 
 export async function createJob(jobName) {
   const newJob = new Job({ name: jobName });
@@ -35,4 +56,52 @@ export async function startInterview(interviewId) {
   ).exec();
 
   return startedInterview;
+}
+
+export async function submitVideo(file) {
+  const tempPath = file.path;
+  const fileName = uuid4();
+  const targetPath = `./upload/${fileName}.mp4`;
+  const audioOutputPath = `./upload/${fileName}.mp3`;
+
+  await new Promise((resolve, reject) => {
+    if (path.extname(file.originalname).toLowerCase() === ".mp4") {
+      fs.rename(tempPath, targetPath, async (err) => {
+        if (err) throw Error(err);
+
+        await new Promise((resolve, reject) => {
+          ffmpeg()
+            .input(targetPath)
+            .toFormat("mp3")
+            .saveToFile(audioOutputPath)
+            .on("end", () => {
+              resolve();
+            })
+            .on("error", (err) => {
+              return reject(new Error(err));
+            });
+        });
+
+        const [response] = await client.recognize({
+          config: config,
+          audio: {
+            content: fs.readFileSync(audioOutputPath).toString("base64"),
+          },
+        });
+
+        const transcription = response.results
+          .map((result) => {
+            return result.alternatives[0].transcript;
+          })
+          .join("\n");
+
+        resolve();
+      });
+    } else {
+      fs.unlink(tempPath, (err) => {
+        if (err) return reject(Error(err));
+      });
+    }
+  });
+  return "success";
 }
